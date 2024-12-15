114e3bcf4f97012ca7edba071ea49489f3ec2f9a
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-15 10:58:16 +0100

    were getting somewhere

--- mongo1/app.js
+++ mongo1/app.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const User = require("./models/userModel");
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({"msg": "hello world"})
+});
+
+app.get("/users/all", async (req, res) => {
+    let _users = await User.find({});
+    return res.json({"users": _users});
 });
 
 module.exports = app;
